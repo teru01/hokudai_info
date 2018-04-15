d2b9a973bce082444bf0d6e26c079d81fd62b448
teruya <abas.trace.on@gmail.com>
2018-04-15 12:21:49 +0900

    期限迫っていたら警告を出す機能を追加

--- practice.py
+++ practice.py
@@ -1,18 +1,37 @@
+import datetime
+
 import requests
 import keyring as kr
 from bs4 import BeautifulSoup
 
 
+def is_due_back_soon(ret_date):
+    REMINDER_DAYS = 3
+    ret_date_formatted = datetime.datetime.strptime(ret_date, "%Y.%m.%d")
+    now = datetime.datetime.today()
+    if (ret_date_formatted - now).days < REMINDER_DAYS:
+        return True
+
+
 def main():
-    elmsid = kr.get_password('elmsid', 'elmsid')
-    passwd = kr.get_password('elms', elmsid)
+    _elms_id = kr.get_password('elmsid', 'elmsid')
+    _passwd = kr.get_password('elms', _elms_id)
     payload = {'PSTKBN': '2',
-               'LOGIN_USERID': elmsid,
-               'LOGIN_PASS': passwd, }
+               'LOGIN_USERID': _elms_id,
+               'LOGIN_PASS': _passwd, }
+
     with requests.Session() as s:
         r = s.post('https://opac.lib.hokudai.ac.jp/opac-service/srv_odr_stat.php', data=payload)
         soup = BeautifulSoup(r.text, 'html.parser')
-        print(soup.find_all('table')[3])
+        try:
+            for book_data in soup.find_all('table')[3].find_all('tr')[1:]:
+                title = ''.join(book_data.find_all('td')[6].string.split('/')[:-1])
+                ret_date = book_data.find_all('td')[4].string
+                if is_due_back_soon(ret_date):
+                    print("返却期限が迫っています")
+                    print("{} , 返却期限: {}".format(title, ret_date))
+        except IndexError:
+            pass
 
 if __name__ == '__main__':
     main()
