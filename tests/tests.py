34c020ecfc65c740a6113466d618d128354bcc7a
teruya <abas.trace.on@gmail.com>
2018-04-15 14:23:26 +0900

    is_due_back_soonのテストケースを作成

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,11 @@
 import unittest
-import
+from borrowed_books.borrowed_book import is_due_back_soon
+import datetime
+
+
 class TestIsDueBackSoon(unittest.TestCase):
-    def test_id_due_back_soon(self):
+    def setUp(self):
+        self.today = datetime.datetime.today()
+
+    def test_is_due_back_soon(self):
+        self.assertTrue(is_due_back_soon(self.today))
